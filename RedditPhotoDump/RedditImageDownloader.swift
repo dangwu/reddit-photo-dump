3436ba95c21a1bc0f8b794c3041605557cacde6a
Daniel Wu <dgwu@amazon.com>
2017-02-03 15:43:59 -0800

    Fix threading bugs within RedditImageDownloader

--- RedditPhotoDump/RedditImageDownloader.swift
+++ RedditPhotoDump/RedditImageDownloader.swift
@@ -26,6 +26,9 @@ class RedditImageDownloader {
     
     private var tasks: [URLSessionDataTask]?
     
+    private let serialQueue = DispatchQueue(label: "reddit-image-downloader-serial")
+    private let asyncQueue = DispatchQueue(label: "reddit-image-downloader-async", attributes: .concurrent)
+    
     init(delegate: RedditImageDownloaderDelegate) {
         self.delegate = delegate
         tasks = [URLSessionDataTask]()
@@ -54,61 +57,63 @@ class RedditImageDownloader {
             return
         }
         
-        var redditUrl = url
-        if let pageAfterToken = pageAfterToken {
-            redditUrl = "\(url)?after=\(pageAfterToken)"
-        }
-        
-        guard let requestURL = URL(string: redditUrl) else {
-            return
-        }
-        
-        NSLog("Downloading JSON at url \(redditUrl)")
-        let request = URLRequest(url: requestURL)
-        let task = URLSession.shared.dataTask(with: request, completionHandler: {
-            [weak self]
-            data, response, error in
-            do {
-                guard self?.running ?? false else {
-                    return
-                }
-                guard let data = data else {
-                    return
-                }
-                guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary else {
-                    return
-                }
-                
-                // Parse JSON for URLs to images to download
-                let dataDict = json.object(forKey: "data") as! NSDictionary
-                let childrenArr = dataDict.object(forKey: "children") as! [[String: AnyObject]]
-                for postDict in childrenArr {
-                    for (postKey, postValue) in postDict where postKey == "data" {
-                        for (dataKey, dataValue) in postValue as! [String: AnyObject] where dataKey == "url" {
-                            if let url = dataValue as? String {
-                                // URL to an image found. Download it!
-                                if url.lowercased().contains(".jpg") || url.lowercased().contains(".png") {
-                                    self?.downloadImageAtURL(url: URL(string: url)!)
+        serialQueue.sync {
+            var redditUrl = url
+            if let pageAfterToken = pageAfterToken {
+                redditUrl = "\(url)?after=\(pageAfterToken)"
+            }
+            
+            guard let requestURL = URL(string: redditUrl) else {
+                return
+            }
+            
+            NSLog("Downloading JSON at url \(redditUrl)")
+            let request = URLRequest(url: requestURL)
+            let task = URLSession.shared.dataTask(with: request, completionHandler: {
+                [weak self]
+                data, response, error in
+                do {
+                    guard self?.running ?? false else {
+                        return
+                    }
+                    guard let data = data else {
+                        return
+                    }
+                    guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary else {
+                        return
+                    }
+                    
+                    // Parse JSON for URLs to images to download
+                    let dataDict = json.object(forKey: "data") as! NSDictionary
+                    let childrenArr = dataDict.object(forKey: "children") as! [[String: AnyObject]]
+                    for postDict in childrenArr {
+                        for (postKey, postValue) in postDict where postKey == "data" {
+                            for (dataKey, dataValue) in postValue as! [String: AnyObject] where dataKey == "url" {
+                                if let url = dataValue as? String {
+                                    // URL to an image found. Download it!
+                                    if url.lowercased().contains(".jpg") || url.lowercased().contains(".png") {
+                                        self?.downloadImageAtURL(url: URL(string: url)!)
+                                    }
                                 }
                             }
                         }
                     }
+                    
+                    guard self?.running ?? false else {
+                        return
+                    }
+                    
+                    if let pageAfterToken = dataDict.object(forKey: "after") as? String {
+                        // Download next page of data
+                        self?.downloadUrl(url, pageAfterToken: pageAfterToken)
+                    }
+                } catch let error as NSError {
+                    NSLog("Error downloading json data from reddit: \(error.debugDescription)")
                 }
-                
-                guard self?.running ?? false else {
-                    return
-                }
-                
-                if let pageAfterToken = dataDict.object(forKey: "after") as? String {
-                    // Download next page of data
-                    self?.downloadUrl(url, pageAfterToken: pageAfterToken)
-                }
-            } catch let error as NSError {
-                NSLog("Error downloading json data from reddit: \(error.debugDescription)")
-            }
-        }) 
-        task.resume()
-        tasks?.append(task)
+            }) 
+            task.resume()
+            tasks?.append(task)
+        }
     }
     
     private func downloadImageAtURL(url: URL) {
@@ -116,44 +121,51 @@ class RedditImageDownloader {
             return
         }
         
-        totalImagesCount += 1
-        
-        NSLog("Downloading image at url \(url)")
-        let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request, completionHandler: {
-            [weak self]
-            data, response, error in
-            
-            guard self?.running ?? false else {
-                return
-            }
-            
-            let time = String(CACurrentMediaTime())
-            let fileName = "\(time).jpg"
-            
-            guard let data = data,
-                let image = UIImage(data: data),
-                let fileURL = ImageUtil.urlForImage(fileName),
-                let jpgImageData = UIImageJPEGRepresentation(image, 1) else {
-                return
-            }
-            
-            // Save image to disk
-            try? jpgImageData.write(to: fileURL, options: [])
-            
-            // Add downloaded image to camera roll
-            PHPhotoLibrary.addImage(atURL: fileURL)
-            
-            // Notify delegate of new image
-            DispatchQueue.main.async {
+        asyncQueue.async {
+            NSLog("Downloading image at url \(url)")
+            let request = URLRequest(url: url)
+            let task = URLSession.shared.dataTask(with: request, completionHandler: {
+                [weak self]
+                data, response, error in
+                
                 guard self?.running ?? false else {
                     return
                 }
-                self?.delegate?.imageDownloaded(withFileName: fileName)
+                
+                let time = String(CACurrentMediaTime())
+                let fileName = "\(time).jpg"
+                
+                guard let data = data,
+                    let image = UIImage(data: data),
+                    let fileURL = ImageUtil.urlForImage(fileName),
+                    let jpgImageData = UIImageJPEGRepresentation(image, 1) else {
+                    return
+                }
+                
+                // Save image to disk
+                try? jpgImageData.write(to: fileURL, options: [])
+                
+                // Add downloaded image to camera roll
+                PHPhotoLibrary.addImage(atURL: fileURL)
+                
+                // Notify delegate of new image
+                DispatchQueue.main.async {
+                    guard self?.running ?? false else {
+                        return
+                    }
+                    self?.delegate?.imageDownloaded(withFileName: fileName)
+                }
+                
+                self?.serialQueue.sync {
+                    self?.totalImagesCount += 1
+                }
+            }) 
+            task.resume()
+            
+            self.serialQueue.sync {
+                self.tasks?.append(task)
             }
-        }) 
-        task.resume()
-        tasks?.append(task)
+        }
     }
     
     func deleteAllDownloadedFiles() {
